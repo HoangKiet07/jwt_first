8eb58385fd0ea1c50c87c509eb4be84f7fc45903
kiet <trungkiet1301@gmail.com>
2022-11-01 19:58:42 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/jwt_first.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jwt_first {
+
+    public static void main(String[] args) {
+        SpringApplication.run(jwt_first.class, args);
+    }
+
+}
